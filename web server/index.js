2cf093ad1a3985e75a306f5659b7c15762f08fc1
maralpbilisim <72076365+maralpbilisim@users.noreply.github.com>
2022-08-26 13:19:25 +0300

    web server eklnedi

---
+++ web server/index.js
@@ -0,0 +1,27 @@
+const http=require('http');
+
+const server=http.createServer((req,res) => {
+    
+    const url=req.url
+     if (url==='/'){
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write('<h2>index  sayfasına hoşgeldiniz</h2>')
+    }
+    else if(url==='/hakkimda'){
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write('<h2>hakkimda sayfasına hoşgeldiniz</h2>')
+    }
+    else if(url==='/iletisim'){
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write('<h2>iletisim sayfasına hoşgeldiniz</h2>')
+    }
+    else{
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.write('<h2>404 sayfa bulunamadı</h2>')
+    }
+    res.end()
+});
+const port=5000;
+server.listen(port,()=>{
+    console.log(`sunucu port ${port} de başlatıldı`)
+})
