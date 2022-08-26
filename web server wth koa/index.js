65d7f68bab19d3f986a5315f2a31773ede9a0284
maralpbilisim <72076365+maralpbilisim@users.noreply.github.com>
2022-08-26 17:50:57 +0300

    koa eklendi

--- web server wth koa/index.js
+++ web server wth koa/index.js
@@ -1,1 +1,23 @@
-sasa
+const Koa = require('koa');
+const app = new Koa();
+
+// response
+app.use(ctx => {
+  const url=ctx.path
+  ctx.set("content-type", "text/html")
+  if(url==="/"){
+    ctx.body="<h1>Ana sayfa</h1>"
+  }
+  else if(url==="/about"){
+    ctx.body="<h1>Hakkimizda</h1>"
+  }
+  else if(url==="/contact"){
+    ctx.body="<h1>iletisim</h1>"
+  }
+  else{
+    ctx.body = "<h1>404 not found</h1>";
+    ctx.status = 404;
+  }
+})
+
+app.listen(3000);
